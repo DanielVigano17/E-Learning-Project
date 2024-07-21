16cd3971895a672340363bec1c2f8c343079327a
Daniel Vigano <unb10@toledobrasil.com>
2024-07-21 15:28:48 -0300

    Adicionar arquivos de projeto.

---
+++ E-Learning-Project/Views/Home/Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace E_Learning_Project.Views.Home
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+    }
+}
